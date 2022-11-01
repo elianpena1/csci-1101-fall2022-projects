fd8b2efb64d12a878b0423f3f8ad63db6611660e
Elian <elianpena281@gmail.com>
2022-11-01 10:44:05 -0500

    Add web project

--- section03/hangman/main.py
+++ section03/hangman/main.py
@@ -1,7 +1,19 @@
 import re
+import random
 
 # Get the answer 
-answer = "What's Up, Doc?"
+pool_file = open("hangman-sample-answer-pool.txt")
+
+pool_answers = []
+
+pool_answer_line = pool_file.readline()
+
+while pool_answer_line:
+    pool_answers.append(pool_answer_line)
+
+    pool_answer_line = pool_file.readline()
+
+answer = random.choice(pool_answers)
 
 answer = answer.upper()
 
@@ -24,7 +36,52 @@ letters_guessed = []
 # Let user play the game.
 while current_incorrect_guesses < num_of_incorrect_guesses and False in answer_guessed:
     # Display game status. 
-    print(f"numnber of incorrect guesses remaining: {num_of_incorrect_guesses - current_answer_character}")
+    print(f"number of incorrect guesses remaining: {num_of_incorrect_guesses - current_incorrect_guesses}")
 
-            
-        
+    print("Letters guessed: ", end="")
+
+    for current_letter_guessed in letters_guessed:
+        print(current_letter_guessed, end=" ")
+    
+    print()
+
+    # Display puzzle board.
+    for current_answer_index in range(len(answer)):
+        if answer_guessed[current_answer_index]:
+            print(answer[current_answer_index], end="")
+        else:
+            print("_", end="")
+
+    print()
+
+    # Let the user guess a letter.
+    letter = input("Enter a letter: ")
+
+    letter = letter.upper()
+    
+    # Check if user entered a valid letter.
+    if re.search("^[A-Z]$", letter) and len(letter) == 1 and letter not in letters_guessed:
+        # Insert letter guessed (insertion sort)
+        current_letter_index = 0 
+
+        for current_letters_guessed in letters_guessed: 
+            if letter < current_letters_guessed:
+                break
+
+            current_letter_index += 1 
+
+        letters_guessed.insert(current_letter_index, letter)
+
+        # Check if letter is in the puzzle.
+        if letter in answer:
+            for current_answer_index in range(len(answer)): 
+                if letter == answer[current_answer_index]:
+                    answer_guessed[current_answer_index] = True
+        else:
+            current_incorrect_guesses += 1
+
+# Post game summary.
+if current_incorrect_guesses < num_of_incorrect_guesses:
+    print("Congradulations, you won!")
+else:
+    print(f"Sorry, you lost. The answer was {answer}")
